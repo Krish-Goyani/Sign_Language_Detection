41b33117be5265187fe592241d5c607648be7004
Krish-Goyani <goyani@duck.com>
2024-08-18 15:26:28 +0530

    dependency resolved

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ while True:
             wGap = math.ceil((imgSize - wCal) / 2)
             imgWhite[:, wGap:wCal + wGap] = imgResize
             prediction, index = classifier.getPrediction(imgWhite, draw=False)
-            print(prediction, index)
+            
         else:
             k = imgSize / w
             hCal = math.ceil(k * h)
@@ -37,6 +37,7 @@ while True:
             hGap = math.ceil((imgSize - hCal) / 2)
             imgWhite[hGap:hCal + hGap, :] = imgResize
             prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            
         cv2.rectangle(imgOutput, (x - offset, y - offset-50),
                       (x - offset+90, y - offset-50+50), (255, 0, 255), cv2.FILLED)
         cv2.putText(imgOutput, labels[index], (x, y -26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
